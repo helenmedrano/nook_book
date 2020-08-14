a66619dfb8144d8c8d8963555f77f3d6a852e973
Helen Medrano <helen.medrano@versussystems.com>
2020-08-14 11:45:39 -0700

    Step 9

--- lib/nook_book/acnh/cache.ex
+++ lib/nook_book/acnh/cache.ex
@@ -1,59 +1,53 @@
 defmodule NookBook.ACNH.Cache do
-  alias NookBook.ACNH.API.Client, as: Client
-  alias NookBook.Data.GenericCache, as: Cache
+  alias NookBook.ACNH.Cache.Private
 
   def bugs() do
-    {:bug, :_}
-    |> Cache.filter()
-    |> case do
-      [] ->
-        Client.bugs()
-        |> Enum.map(&Cache.set({:bug, &1["id"]}, &1))
-
-      list ->
-        list
-    end
-    |> Enum.sort_by(fn record -> String.capitalize(record["name"]["name-USen"]) end)
+    Private.list_from_cache(:bug, :bugs)
   end
 
   def bug(id) do
-    {:bug, id}
-    |> Cache.get()
-    |> case do
-      nil ->
-        record = Client.bug(id)
-        Cache.set({:bug, id}, record)
-
-      record ->
-        record
-    end
+    Private.record_from_cache(:bug, :bug, id)
   end
 
   def bug_icon(id) do
-    {:bug_icon, id}
-    |> Cache.get()
-    |> case do
-      nil ->
-        record = Client.bug_icon(id)
-        Cache.set({:bug_icon, id}, record)
-
-      record ->
-        record
-    end
+    Private.record_from_cache(:bug_icon, :bug_icon, id)
   end
 
   def bug_image(id) do
-    {:bug_image, id}
-    |> Cache.get()
-    |> case do
-      nil ->
-        record = Client.bug_image(id)
-        Cache.set({:bug_image, id}, record)
-
-      record ->
-        record
-    end
+    Private.record_from_cache(:bug_image, :bug_image, id)
   end
 
+  defmodule Private do
+    alias NookBook.ACNH.API.Client, as: Client
+    alias NookBook.Data.GenericCache, as: Cache
+
+    def list_from_cache(namespace, function) do
+      {namespace, :_}
+      |> Cache.filter()
+      |> case do
+        [] ->
+          apply(Client, function, [])
+          |> Enum.reject(fn record -> is_nil(record["id"]) end)
+          |> Enum.map(&Cache.set({:bug, &1["id"]}, &1))
+
+        list ->
+          list
+      end
+      |> Enum.sort_by(fn record -> String.capitalize(record["name"]["name-USen"]) end)
+    end
+
+    def record_from_cache(namespace, function, id) do
+      {namespace, id}
+      |> Cache.get()
+      |> case do
+        nil ->
+          record = apply(Client, function, [id])
+          Cache.set({namespace, id}, record)
+
+        record ->
+          record
+      end
+    end
+  end
 end
 
