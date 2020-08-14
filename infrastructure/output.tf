d91580769c7a50c20fdb3ea57718629d9c75899f
Helen Medrano <helen.medrano@versussystems.com>
2020-08-14 16:45:19 -0700

    add more output to terraform

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,6 @@
+variable "sshkey" { default = "helen_aws" }
+variable "bastion_id" { default = "i-0f592d9fe5e38f33c" }
+
 output "libcluster-hosts" {
   value = <<HOSTS
 
@@ -6,4 +9,38 @@ hosts: [
   :"nook_book@${aws_instance.nookbook2.private_ip}"
 ]
 HOSTS
+}
+
+output "sshconfig" {
+  value = <<SSHCONF
+
+Host nookbook_bastion
+  User ec2-user
+  Hostname 18.188.0.20
+  IdentityFile ~/.ssh/helen_aws
+
+Host nookbook1
+  User ec2-user
+  Hostname ${aws_instance.nookbook1.private_ip}
+  ProxyCommand ssh -q -W %h:%p nookbook_bastion
+  IdentityFile ~/.ssh/helen_aws
+
+Host nookbook2
+  User ec2-user
+  Hostname ${aws_instance.nookbook2.private_ip}
+  ProxyCommand ssh -q -W %h:%p nookbook_bastion
+  IdentityFile ~/.ssh/helen_aws
+
+SSHCONF
+}
+
+output "ssh-commands" {
+value = <<SSHCMDS
+
+aws ec2-instance-connect send-ssh-public-key --instance-id ${var.bastion_id} --instance-os-user ec2-user --ssh-public-key file://~/.ssh/${var.sshkey}.pub --availability-zone us-east-2a | cat
+aws ec2-instance-connect send-ssh-public-key --instance-id ${aws_instance.nookbook1.id} --instance-os-user ec2-user --ssh-public-key file://~/.ssh/${var.sshkey}.pub --availability-zone us-east-2c | cat
+aws ec2-instance-connect send-ssh-public-key --instance-id ${aws_instance.nookbook2.id} --instance-os-user ec2-user --ssh-public-key file://~/.ssh/${var.sshkey}.pub --availability-zone us-east-2c | cat
+
+
+SSHCMDS
 }
